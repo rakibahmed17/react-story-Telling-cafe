4eca3d6edd0d8792ec9e656aff8dc00277894ccb
rakibahmed17 <rakibahmed000111@gmail.com>
2023-12-19 19:55:17 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+    return (
+        <>
+            <h1 className="text-3xl bg-red-700">React knowledge cafe</h1>
+        </>
+    );
+}
+
+export default App;
